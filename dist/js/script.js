8699fc038d87c6917d36bdf0e957afe5ecc5d917
Daniil Zhilin <lasermoai@gmail.com>
2020-02-01 03:42:44 +0300

    Minor fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 /*! carousel-solution v0.1.0 | (c) 2020 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
 $(document).ready((function () {
   $(".splash-hide-button").click((function () {
-    $( ".splash-screen" ).delay(5000).fadeOut();
+    $(".splash-screen").delay(5000).fadeOut();
   }));
 })); 
